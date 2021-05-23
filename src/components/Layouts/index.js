f39b892a0e8add3638ba9f4cf8a034d928fc7978
unknown <rojandhimal1@gmail.com>
2021-05-24 00:34:06 +0545

    Project Setup

---
+++ src/components/Layouts/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/index'
+
+const Layout = (props) => {
+    return (
+        <>
+            <Header />
+            {props.children}
+        </>
+    )
+}
+
+export default Layout;
---
+++ src/containers/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            THis is homapegae
+        </div>
+    )
+}
+
+export default Home
